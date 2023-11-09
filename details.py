eee03c8d1de6f43cbe5efef94d9aa9d08dd6a944
tiger7815 <141488144+tiger7815@users.noreply.github.com>
2023-11-10 03:39:03 +0530

    Update details.py

--- details.py
+++ details.py
@@ -1,5 +1,5 @@
-api_id = 29410389
-api_hash = "0c716764715886f6641477ffbb63e1ee"
-bot_token = "5946934925:AAGoALZ3-Cr8hQaBdzHhmW3pknt-h1LVVpI"
-sudo_groups = -1001777454744
+api_id = 22609670
+api_hash = "3506d8474ad1f4f5e79b7c52a5c3e88d"
+bot_token = "6711741427:AAGrYQvfpms_7_wIIvHfnOWzUwkQF_2uKlM"
+sudo_groups = -4018525559
 
